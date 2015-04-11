cd9076874d69ed139c7813802e95d0bcf1e8d349
Ed <edcarlisle4@gmail.com>
2015-04-11 02:29:21 -0400

    added simics functionality up to creating checkpoints unified some bdi and simics functions

--- DrSEUS.py
+++ DrSEUS.py
@@ -12,6 +12,7 @@ import scp
 import subprocess
 import signal
 
+import os
 import time
 import random
 
@@ -22,8 +23,13 @@ class dut:
         self.serial = serial.Serial(port=serial_port,
                                     baudrate=baud_rate,
                                     timeout=None, rtscts=True)
-        self.prompt = prompt
+        self.prompt = prompt+' '
         self.ip_address = ip_address
+
+    def close(self):
+        # TODO: new solution instead of sync to preserver filesystem, maybe halt?
+        self.command('sync')
+        self.serial.close()
 
     def send_files(self, files):
         ssh = paramiko.SSHClient()
@@ -42,23 +48,23 @@ class dut:
         ssh.close()
 
     # TODO: add timeout, delete print statement
-    def read_until(self, string):
+    def read_until(self, string, debug=True):
         buff = ''
         while True:
             char = self.serial.read()
-            print (char, end='')
+            if debug:
+                print(char, end='')
             buff += char
             if buff[-len(string):] == string:
                 return buff
 
-    def command(self, command):
+    def command(self, command, debug=True):
         self.serial.write(command+'\n')
-        self.read_until(self.prompt)
+        return self.read_until(self.prompt, debug)
 
     def do_login(self):
         self.read_until('login: ')
         self.command('root')
-
         self.command('mkdir .ssh')
         self.command('touch .ssh/authorized_keys')
         self.command('echo \"ssh-rsa '+self.rsakey.get_base64() +
@@ -77,6 +83,10 @@ class bdi:
         if not self.reset_dut():
             print('error resetting dut')
             sys.exit()
+
+    def close(self):
+        self.command('quit')
+        self.telnet.close()
 
     def ready(self):
         if self.telnet.expect(['P2020>'], timeout=10)[0] < 0:
@@ -158,35 +168,49 @@ class simics:
                                         'simplifi/DrSEUS.simics'],
                                        cwd=simics_workspace,
                                        stdin=subprocess.PIPE,
-                                       stdout=subprocess.PIPE)
-        self.simics.stdin.write('run\n')
-        # TODO: add timeout
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE)
+        self.continue_dut()
+        # TODO: add timeout and remove print
         while True:
             line = self.simics.stdout.readline()
+            print(line, end='')
             if 'pseudo device opened: /dev/pts/' in line:
                 serial_port = line.split(':')[1].strip()
                 break
+        self.read_until('simics> ')
         self.dut = dut(dut_ip_address, serial_port, baud_rate=38400)
         self.do_uboot()
 
-    def halt_dut(self):
+    def close(self):
         self.simics.send_signal(signal.SIGINT)
+        self.simics.stdin.write('quit\n')
+        self.simics.terminate()
+
+    def halt_dut(self, debug=True):
+        self.simics.send_signal(signal.SIGINT)
+        return self.read_until('simics> ', debug)
 
     def continue_dut(self):
         self.simics.stdin.write('run\n')
+        # self.read_until('running>', debug=True)
 
-    # TODO: add timeout
-    def read_until(self, string):
+    # TODO: add timeout and remove print
+    def read_until(self, string, debug=True):
         buff = ''
         while True:
-            line = self.simics.stdout.readline()
-            buff += line
-            if string in line:
+            char = self.simics.stdout.read(1)
+            if debug:
+                print(char, end='')
+            buff += char
+            if buff[-len(string):] == string:
+                if debug:
+                    print('')
                 return buff
 
-    def command(self, command):
+    def command(self, command, debug=True):
         self.simics.stdin.write(command+'\n')
-        self.read_until('simics>')
+        return self.read_until('simics> ', debug)
 
     def do_uboot(self):
         self.dut.read_until('autoboot: ')
@@ -194,6 +218,7 @@ class simics:
         self.halt_dut()
         self.command('$system.soc.phys_mem.load-file ' +
                      '$initrd_image $initrd_addr')
+        self.command('$dut_system = $system')
         self.continue_dut()
         self.dut.serial.write('setenv ethaddr 00:01:af:07:9b:8a\n' +
                               # 'setenv ipaddr 10.10.0.100\n' +
@@ -206,6 +231,17 @@ class simics:
                               'setenv bootargs root=/dev/ram rw console=' +
                               '$consoledev,$baudrate $othbootargs\n' +
                               'bootm ef080000 10000000 ef040000\n')
+
+    def create_checkpoints(self, target_app, cycles, num_checkpoints):
+        if not os.path.exists('../simics-workspace/gold_checkpoints'):
+            os.mkdir('../simics-workspace/gold_checkpoints')
+        step_cycles = cycles / num_checkpoints
+        self.halt_dut()
+        self.dut.serial.write('./'+target_app+'\n')
+        for checkpoint in xrange(num_checkpoints):
+            self.command('run-cycles '+str(step_cycles))
+            self.command('write-configuration gold_checkpoints/checkpoint-'+str(checkpoint))
+            # TODO: merge and delete partial checkpoints
 
 
 class simics_p2020(simics):
@@ -232,20 +268,12 @@ class fault_injector:
                              ' netmask 255.255.255.0')
 
     def exit(self):
-        if self.debugger.simics:
-            self.debugger.halt_dut()
-            self.debugger.command('quit')
-            self.debugger.simics.terminate()
-        else:
-            self.debugger.command('quit')
-            self.debugger.telnet.close()
-            # TODO: new solution instead of sync to preserver filesystem, maybe halt?
-            self.dut.command('sync')
+        self.debugger.close()
         self.dut.serial.close()
         sys.exit()
 
     def setup_campaign(self, directory, application, arguments='',
-                       additional_files=None):
+                       additional_files=None, num_checkpoints=100):
         if arguments:
             self.target_app = application+' '+arguments
         else:
@@ -259,32 +287,46 @@ class fault_injector:
         self.exec_time = self.time_application(5)
         self.dut.get_file('result.dat')
 
+        if self.debugger.simics:
+            self.debugger.create_checkpoints(self.target_app, self.exec_time, num_checkpoints)
+
     # TODO: get cycles if using simics
     def time_application(self, iterations):
-        start = time.time()
-        for i in xrange(iterations):
-            self.dut.command('./'+self.target_app)
-        return (time.time() - start) / iterations
-
-    def main(self):
-        if self.debugger.simics is None:
-            injection_time = random.uniform(0, self.exec_time)
-            print('injection at: '+str(injection_time))
-            self.dut.serial.write('./'+self.application+' '+self.arguments+'\n')
-
-            time.sleep(injection_time)
-            if not self.debugger.halt_dut():
-                print('error halting dut')
-                sys.exit()
-
-            print(self.debugger.get_dut_regs())
-
+        if self.debugger.simics:
+            for i in xrange(iterations-1):
+                self.dut.command('./'+self.target_app)
+            self.debugger.halt_dut()
+            start_cycles = self.debugger.command('print-time').split('\n')[-2].split()[2]
             self.debugger.continue_dut()
-            self.dut.read_until(self.dut.serial_prompt)
-# try:
+            self.dut.command('./'+self.target_app)
+            self.debugger.halt_dut()
+            end_cycles = self.debugger.command('print-time').split('\n')[-2].split()[2]
+            self.debugger.continue_dut()
+            return int(end_cycles) - int(start_cycles)
+        else:
+            start = time.time()
+            for i in xrange(iterations):
+                self.dut.command('./'+self.target_app)
+            return (time.time() - start) / iterations
+
+    # def main(self):
+    #     if self.debugger.simics is None:
+    #         injection_time = random.uniform(0, self.exec_time)
+    #         print('injection at: '+str(injection_time))
+    #         self.dut.serial.write('./'+self.application+' '+self.arguments+'\n')
+
+    #         time.sleep(injection_time)
+    #         if not self.debugger.halt_dut():
+    #             print('error halting dut')
+    #             sys.exit()
+
+    #         print(self.debugger.get_dut_regs())
+
+    #         self.debugger.continue_dut()
+    #         self.dut.read_until(self.dut.serial_prompt)
+
 DrSEUS = fault_injector(dut_ip_address='10.10.0.100', use_simics=True)
 DrSEUS.setup_campaign('../simics-workspace/simicsfs', 'ppc_fi_mm_omp')
 # DrSEUS = fault_injector()
 # DrSEUS.setup_campaign('../FIapps', 'ppc_fi_mm_omp')
-# finally:
 DrSEUS.exit()
