f25a339080ef79ce20fdd68dd129fe7e53d2b9a7
Ed <edcarlisle4@gmail.com>
2015-05-16 19:20:26 -0400

    added simics-vexpress-a9x4 repo to setup_simics script

--- setup_simics.sh
+++ setup_simics.sh
@@ -8,3 +8,4 @@ mkdir simics-workspace
 cd simics-workspace
 /opt/simics/simics-4.8/simics-4.8*/bin/workspace-setup
 git clone git@gitlab.hcs.ufl.edu:F4/simics-p2020rdb
+git clone git@gitlab.hcs.ufl.edu:F4/simics-vexpress-a9x4
