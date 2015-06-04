608de8c5fc601d76e08c67e981c5bb97c7b4633b
Ed <edcarlisle4@gmail.com>
2015-06-04 13:46:18 -0400

    added chartit patch for simplejson

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,3 +2,7 @@
 
 sudo apt-get install python-pip
 sudo pip install paramiko scp termcolor django django-chartit simplejson
+dir=$(pwd)
+pushd /usr/local/lib/python2.7/dist-packages/chartit
+sudo patch -p1 -i "${dir}/chartit.patch"
+popd
