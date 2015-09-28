a3f1e76946af7c72fec6e466758a9dc863006dd9
Ed <edcarlisle4@gmail.com>
2015-09-28 17:31:53 -0400

    added script to package drseus and remove source code

---
+++ package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir simics-workspace
+pushd simics-workspace
+git clone git@gitlab.hcs.ufl.edu:F4/simics-p2020rdb
+# git clone git@gitlab.hcs.ufl.edu:F4/simics-a9x2
+popd
+
+rm .gitignore setup_apps.sh setup_bdi_tftp.sh setup_simics.sh
+rm -rf .git
+
+python -m compileall -l .
+rm ./*.py
+chmod +x drseus.pyc
+
+pushd django-logging/drseus_logging
+python -m compileall -l .
+rm ./*.py
+popd
+
+rm package.sh
