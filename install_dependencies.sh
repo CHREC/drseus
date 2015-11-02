ec51b013f6b59dc10835d47da173fb9f63bd3c6c
Ed <edcarlisle4@gmail.com>
2015-11-02 15:27:31 -0500

    added python-dev to dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,5 +3,5 @@
 sudo usermod -a -G dialout "$USER"
 echo "added $USER to the dialout group (allows non-root access to serial ports)"
 echo 'logout for this change to take effect'
-sudo apt-get install python-pip
+sudo apt-get install python-dev python-pip
 sudo pip install paramiko scp termcolor django simplejson django_tables2 django-filter numpy
