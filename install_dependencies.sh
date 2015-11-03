05a5c438b15a20a0330c8850aa2130e05593cb12
Ed <edcarlisle4@gmail.com>
2015-11-02 22:09:18 -0500

    removed dialout group from install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-sudo usermod -a -G dialout "$USER"
-echo "added $USER to the dialout group (allows non-root access to serial ports)"
-echo 'logout for this change to take effect'
 sudo apt-get install python-dev python-pip
 sudo pip install paramiko scp termcolor django simplejson django_tables2 django-filter numpy
