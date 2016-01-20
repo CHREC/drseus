0c7319d7f86a0aae6233acc66b9fa556a6ffdeec
Ed <edcarlisle4@gmail.com>
2016-01-20 17:04:41 -0500

    cleanup on aisle 6

--- package.sh
+++ package.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
 
-mkdir simics-workspace
-pushd simics-workspace
-git clone git@gitlab.hcs.ufl.edu:F4/simics-p2020rdb
-pushd simics-p2020rdb
-rm -rf .git
-popd
-git clone git@gitlab.hcs.ufl.edu:F4/simics-a9x2
-pushd simics-a9x2
-rm -rf .git
-popd
-popd
-
-rm .gitignore setup_apps.sh setup_bdi_tftp.sh setup_simics_workspace.sh
-rm -rf .git
+set -e
 
 python3 simics_config.py
 python3 -m compileall -l -b .
-mv lextab.py lextab.bak
-mv parsetab.py parsetab.bak
 rm -rf __pycache__
 rm ./*.py
-mv lextab.bak lextab.py
-mv parsetab.bak parsetab.py
 touch drseus.sh
 printf '#!/bin/bash\npython3 drseus.pyc "$@"' > drseus.sh
 chmod +x drseus.sh
@@ -33,4 +16,6 @@ rm -rf __pycache__
 rm ./*.py
 popd
 
-rm package.sh
+rm -rf .git
+rm .sublimelinterrc .gitignore setup_apps.sh setup_simics_workspace.sh \
+   package.sh
--- setup_bdi_tftp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pushd ~/
-git clone git@gitlab.hcs.ufl.edu:F4/bdi3000-tftp.git
-pushd bdi3000-tftp
-./setup_bdi_tftp.sh
-popd
-popd
