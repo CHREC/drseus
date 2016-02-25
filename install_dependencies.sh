ef61af0b012f6729c4b72ad3b0bac778b6dab878
Ed <edcarlisle4@gmail.com>
2016-02-25 00:30:09 -0500

    added progressbar to dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,5 +5,5 @@ set -e
 sudo apt-get update
 sudo apt-get install --yes libpq-dev openocd postgresql python3-dev python3-pip
 sudo pip3 install --upgrade django django-filter django_tables2 numpy paramiko \
-                            ply psycopg2 pyserial pyudev scp termcolor \
-                            terminaltables
+                            ply progressbar2 psycopg2 pyserial pyudev scp \
+                            termcolor terminaltables
