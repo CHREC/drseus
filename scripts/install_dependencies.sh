03488737a47dec355e82f304ba03f63cdf59e27e
Ed <edcarlisle4@gmail.com>
2016-05-26 15:23:32 -0400

    added options simics installation to dependencies script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,19 @@
-#!/bin/sh
-
-set -e
+#!/bin/sh -e
 
 sudo apt-get update
 sudo apt-get install --yes libpq-dev minicom openocd postgresql python3-dev \
                            python3-pip
 sudo pip3 install --upgrade virtualenv
+
+printf 'install simics? [y/N]: '
+read -r simics
+case "$simics" in
+    Y*|y*)
+        git clone git@gitlab.hcs.ufl.edu:F4/setup-simics.git
+        (
+            cd setup-simics
+            ./setup_simics.sh
+        )
+        rm -rf setup-simics
+        ;;
+esac
