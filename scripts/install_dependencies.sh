414c512a2de4dd5341a9d336dd01125a06507f55
seth roffe <ssroffe@gmail.com>
2018-11-16 20:56:28 -0500

    added support for fpu injection for arm cortex a9

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,15 +6,15 @@ sudo apt-get install --yes libpq-dev libssl-dev minicom postgresql python3-dev \
 sudo pip3 install --upgrade virtualenv
 
 sudo apt-get install --yes automake libtool libusb-1.0-0-dev pkg-config
-#git clone git://git.code.sf.net/p/openocd/code openocd
-#(
-#    cd openocd
-#    git fetch http://openocd.zylin.com/openocd refs/changes/21/4421/7 && git checkout FETCH_HEAD
-#    git checkout tags/v0.10.0
-#    ./bootstrap
-#    ./configure --enable-ftdi
-#    make
-#)
+git clone git://git.code.sf.net/p/openocd/code openocd
+(
+    cd openocd
+    git fetch http://openocd.zylin.com/openocd refs/changes/21/4421/7 && git checkout FETCH_HEAD
+    git checkout tags/v0.10.0
+    ./bootstrap
+    ./configure --enable-ftdi
+    make
+)
 sudo groupadd usb
 echo 'SUBSYSTEMS=="usb", ACTION=="add", MODE="0664", GROUP="usb"' | \
     sudo tee -a /etc/udev/rules.d/99-usbftdi.rules
