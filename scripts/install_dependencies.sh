118109dcd5728640f1bc1956c0a4a6a61b3666c1
Seth Roffe <ssroffe@gmail.com>
2021-03-14 12:28:18 -0400

    Fixed bug in install dependencies with openocd

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -25,15 +25,17 @@ sudo apt-get install --yes libpq-dev libssl-dev minicom postgresql python3-dev \
 sudo pip3 install --upgrade virtualenv
 
 sudo apt-get install --yes automake libtool libusb-1.0-0-dev pkg-config
-git clone git://git.code.sf.net/p/openocd/code openocd
-(
-    cd openocd
-    git fetch http://openocd.zylin.com/openocd refs/changes/21/4421/7 && git checkout FETCH_HEAD
-    git checkout tags/v0.10.0
-    ./bootstrap
-    ./configure --enable-ftdi
-    make
-)
+if [ -d "openocd" ]; then
+    git clone git://git.code.sf.net/p/openocd/code openocd
+    (
+        cd openocd
+        git fetch http://openocd.zylin.com/openocd refs/changes/21/4421/7 && git checkout FETCH_HEAD
+        git checkout tags/v0.10.0
+        ./bootstrap
+        ./configure --enable-ftdi --disable-werror
+        make
+    )
+fi
 sudo groupadd usb
 echo 'SUBSYSTEMS=="usb", ACTION=="add", MODE="0664", GROUP="usb"' | \
     sudo tee -a /etc/udev/rules.d/99-usbftdi.rules
